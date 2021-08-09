0fa621c02c1598f529a2afa3a50aa92172553030
jose <jose.martinez@ufrontera.cl>
2021-08-09 18:01:26 -0400

    fix function name js

--- www/mathcalculator.js
+++ www/mathcalculator.js
@@ -8,6 +8,6 @@ module.exports.add = function (arg0, success, error) {
     exec(success, error, 'MathCalculator', 'add', [arg0])
 };
 
-module.exports.add = function (arg0, success, error) {
+module.exports.substract = function (arg0, success, error) {
     exec(success, error, 'MathCalculator', 'substract', [arg0])
 };
